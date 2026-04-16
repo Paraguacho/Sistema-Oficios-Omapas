a610af4be1645d2304c3ae20d85d5ccd96d0149b
Paraguacho <sebastiansoq03@hotmail.com>
2026-04-16 15:43:47 -0700

    Response para eliminar token de localstorage

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -13,4 +13,20 @@ api.interceptors.request.use((config)=>{
     return config;
 })
 
+api.interceptors.response.use(
+    (response) => {
+        //Si funciona, regresa response
+        return response;
+    },
+    (error) => {
+        //Si el sv tira 401 
+        if(error.response && error.response.status === 401){
+            localStorage.removeItem('token');
+            //direccion a loin
+            window.location.href = '/login';
+        }
+        return Promise.reject(error);
+    }
+)
+
 export default api;
